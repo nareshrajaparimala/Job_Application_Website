cfbebeef98dd09137bbda009d081d9f0afb76771
Naresh.R <nareshrajaparimala000@gmail.com>
2025-09-16 11:09:31 +0530

    i had made the change in the axios and cors to spacfic domain

--- server/server.js
+++ server/server.js
@@ -31,8 +31,10 @@ const app = express();
 // Middleware configuration
 app.use(express.json());
 app.use(cors({
-  origin: '*', // Replace '*' with specific frontend domain for better security
-  credentials: true
+  origin: ['https://mytechz.in', 'https://www.mytechz.in', 'http://localhost:3000', 'http://localhost:5173'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 // Basic route
