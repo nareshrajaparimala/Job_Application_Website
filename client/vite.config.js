b8cb41bb5a25230ed739894d52f641c0c493f8d8
Naresh.R <nareshrajaparimala000@gmail.com>
2025-09-28 12:00:00 +0530

    clone of repo

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  // remover the following line if you don't need to use the proxy for mobile view
-  server: {
-    host: true,
-    allowedHosts: ['.ngrok-free.app', '.ngrok.io', 'localhost'],
-  },
-  // Uncomment the following lines if you want to use the proxy for mobile view
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom']
+        }
+      }
+    }
+  }
 })
